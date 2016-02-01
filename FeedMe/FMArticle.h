9f8e4a677d813587b1ed89faaea20987c262831a
keitzer <ogorek@usc.edu>
2016-02-01 14:16:03 -0500

    article and article VC files created

---
+++ FeedMe/FMArticle.h
@@ -0,0 +1,13 @@
+//
+//  FMArticle.h
+//  FeedMe
+//
+//  Created by Alex Ogorek on 2/1/16.
+//  Copyright © 2016 AFCodeTest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMArticle : NSObject
+
+@end
---
+++ FeedMe/FMArticleViewController.m
@@ -0,0 +1,13 @@
+//
+//  FMArticleViewController.m
+//  FeedMe
+//
+//  Created by Alex Ogorek on 2/1/16.
+//  Copyright © 2016 AFCodeTest. All rights reserved.
+//
+
+#import "FMArticleViewController.h"
+
+@implementation FMArticleViewController
+
+@end
