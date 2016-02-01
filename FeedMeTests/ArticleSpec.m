35dec755fb767d6ab595eda585eed40d2660bc73
keitzer <ogorek@usc.edu>
2016-02-01 18:30:24 -0500

    beginning Article tests added

--- FeedMeTests/ArticleSpec.m
+++ FeedMeTests/ArticleSpec.m
@@ -13,6 +13,32 @@
 SPEC_BEGIN(Article)
 
 describe(@"Article", ^{
+	__block FMArticle *article;
+	__block UIImage *image;
+	
+	beforeEach(^{
+		image = [UIImage imageNamed:@"placeholder"];
+		article = [[FMArticle alloc] initWithTitle:@"TestTitle" summary:@"TestSummary" htmlSummary:@"TestHTMLSummary" link:@"TestLink" andImage:image];
+	});
+	
+	afterEach(^{
+		article = nil;
+	});
+	
+	context(@"after initialization", ^{
+		it(@"should exist", ^{
+			[[article shouldNot] beNil];
+		});
+		
+		it(@"should contain all elements", ^{
+			[[article.articleTitle should] equal:@"TestTitle"];
+			[[article.articleHTMLSummary should] equal:@"TestHTMLSummary"];
+			[[article.articleLink should] equal:@"TestLink"];
+			[[article.articleSummary should] equal:@"TestSummary"];
+			[[article.articleImage should] equal:image];
+		});
+	});
+	
 	
 });
 
