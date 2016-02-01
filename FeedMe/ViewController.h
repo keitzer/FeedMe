1d7bb18da885cb4174f7d5e0bea0f4d8a28e9f92
keitzer <ogorek@usc.edu>
2016-02-01 13:47:18 -0500

    Initial Commit

---
+++ FeedMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeedMe
+//
+//  Created by Alex Ogorek on 2/1/16.
+//  Copyright © 2016 AFCodeTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FeedMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FeedMe
+//
+//  Created by Alex Ogorek on 2/1/16.
+//  Copyright © 2016 AFCodeTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
