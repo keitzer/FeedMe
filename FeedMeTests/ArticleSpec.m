c72431d6ffe1ec25a462db77f7dd78f5f153e7d1
keitzer <ogorek@usc.edu>
2016-02-01 18:25:27 -0500

    article test class added

---
+++ FeedMeTests/ArticleSpec.m
@@ -0,0 +1,19 @@
+//
+//  ArticleSpec.m
+//  FeedMe
+//
+//  Created by Alex Ogorek on 2/1/16.
+//  Copyright © 2016 AFCodeTest. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Kiwi.h"
+#import "FMArticle.h"
+
+SPEC_BEGIN(Article)
+
+describe(@"Article", ^{
+	
+});
+
+SPEC_END
