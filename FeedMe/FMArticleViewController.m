b064e9ef960fd9edf3a27176bc54d4f1978735ab
keitzer <ogorek@usc.edu>
2016-02-01 23:50:02 -0500

    web view loading URL in article VC

--- FeedMe/FMArticleViewController.m
+++ FeedMe/FMArticleViewController.m
@@ -40,6 +40,46 @@
 	self.articleImageView.image = self.article.articleImage ? self.article.articleImage : [UIImage imageNamed:@"placeholder"];
 	self.articleImageView.layer.cornerRadius = 8;
 	self.articleImageView.layer.masksToBounds = YES;
+	
+	self.navigationItem.title = self.article.articleTitle;
+	
+	[self startLoadingWebContent];
+}
+
+-(void)startLoadingWebContent {
+	if (self.article.articleLink) {
+		[self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:self.article.articleLink]]];
+	}
+	else {
+		[self.webView loadHTMLString:self.article.articleHTMLSummary baseURL:nil];
+	}
+	self.webView.delegate = self;
+	
+	self.finishedWithInitialLoading = NO;
+}
+
+#pragma mark - Web View delegate methods
+
+-(void)webViewDidStartLoad:(UIWebView *)webView {
+	if (!self.finishedWithInitialLoading) {
+		[SVProgressHUD showWithStatus:@"Loading..."];
+	}
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+	self.finishedWithInitialLoading = YES;
+	[SVProgressHUD dismiss];
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+	if (!self.finishedWithInitialLoading) {
+		[SVProgressHUD dismiss];
+		
+		NSLog(@"error: %@", error.description);
+		self.webView.hidden = YES;
+	}
+	
+	[self.webView stopLoading];
 }
 
 @end
